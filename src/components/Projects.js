0519cf7c82ca7ead6d08fe03007bbc193e1b2473
unknown <nerwinalamas@gmail.com>
2023-07-01 19:16:59 +0800

    changes in project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Gavs from "../assets/gavs.png";
 import netflixClone from "../assets/netflix-clone.png";
+import neckflix from "../assets/neckflix.png";
 import product from "../assets/product-preview.png";
 import notes from "../assets/notes.png";
+import todolist from "../assets/Todolist.png";
 import {
   FaJava,
   FaReact,
@@ -12,7 +14,13 @@ import {
   FaNodeJs,
   FaBootstrap,
 } from "react-icons/fa";
-import { SiFirebase, SiExpress, SiMongodb } from "react-icons/si";
+import {
+  SiFirebase,
+  SiExpress,
+  SiMongodb,
+  SiVite,
+  SiTailwindcss,
+} from "react-icons/si";
 import { DiAndroid } from "react-icons/di";
 
 const Projects = () => {
@@ -23,46 +31,53 @@ const Projects = () => {
           Projects
         </h1>
         <div className="md:grid grid-cols-2 gap-6 lg:grid-cols-3">
+        {/* <div className="flex flex-col items-center justify-center"> */}
           {/* Project 1 */}
           <div className="my-10">
             <div className="w-70 content-center">
-              <div>
+              <a
+                href="https://todolist-nerwinalamas.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <img
-                  src={Gavs}
-                  alt="GAVS Smart Attendance App"
-                  className="rounded-lg transform hover:scale-110 transition duration-300 md:h-25"
+                  src={todolist}
+                  alt="Todolist"
+                  className="rounded-lg transform hover:scale-110 transition duration-300"
                 />
-              </div>
+              </a>
               <div className="px-3">
-                <h2 className="text-nameColor font-medium text-xl text-center py-5">
-                  GAVS Smart Attendance App
+                <h2 className="text-nameColor font-medium text-xl text-center pt-5 pb-3">
+                  Todolist
                 </h2>
                 <p className="text-textColor text-md max-w-sm">
-                  An Attendance App that gathers student attendance within the
-                  Institution. since social distancing is strictly implemented.
-                  We use a QR code to collect student data.
+                  A simple Todolist, users can create, read, update, and
+                  delete tasks
                 </p>
                 <div className="py-4 flex">
-                  <FaJava
-                    title="Java"
+                  <FaReact
+                    title="ReactJS"
                     className="text-titleColor text-4xl mr-2 hover:text-btnColor"
                   />
-                  <DiAndroid
-                    title="Android Studio"
+                  <SiVite
+                    title="Vite"
                     className="text-titleColor text-4xl mr-2 hover:text-btnColor"
                   />
-                  <SiFirebase
-                    title="Firebase"
+                  <FaJsSquare
+                    title="Vite"
+                    className="text-titleColor text-4xl mr-2 hover:text-btnColor"
+                  />
+                  <SiTailwindcss
+                    title="Vite"
                     className="text-titleColor text-4xl mr-2 hover:text-btnColor"
                   />
                 </div>
                 <p className="text-textColor">
-                  Java, XML, Android Studio & Firebase
+                  Vite, React, JavaScript, Tailwind & React-Icons
                 </p>
-
                 <div className="mt-2">
                   <a
-                    href="https://github.com/nerwinalamas/GAVS-Smart-Attendance-App"
+                    href="https://github.com/nerwinalamas/Todolist"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -70,12 +85,15 @@ const Projects = () => {
                       Repo
                     </button>
                   </a>
-
-                  {/* No Live Demo */}
-
-                  {/* <button className="bg-nameColor rounded py-2 px-6 mt-3 text-backgroundColor font-semibold hover:bg-btnColor hover:text-nameColor">
-                  Demo
-                </button> */}
+                  <a
+                    href="https://todolist-nerwinalamas.vercel.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="bg-nameColor rounded py-2 px-6 mt-3 text-backgroundColor font-semibold mr-3 hover:bg-btnColor hover:text-nameColor">
+                      Demo
+                    </button>
+                  </a>
                 </div>
               </div>
             </div>
@@ -84,20 +102,25 @@ const Projects = () => {
           {/* Project 2 */}
           <div className="my-10">
             <div className="w-70 content-center">
-              <div>
+              <a
+                href="https://netflix-clone-acifr52kq-nerwinalamas.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <img
-                  src={netflixClone}
+                  src={neckflix}
                   alt="Netflix Clone"
-                  className="rounded-lg  transform hover:scale-110 transition duration-300  md:h-30"
+                  className="rounded-lg  transform hover:scale-110 transition duration-300"
                 />
-              </div>
+              </a>
               <div className="px-3">
-                <h2 className="text-nameColor font-medium text-xl text-center py-5">
+                <h2 className="text-nameColor font-medium text-xl text-center  pt-5 pb-3">
                   Netflix Clone
                 </h2>
-                <p className="text-textColor text-md max-w-sm">
-                  Clone of Movie Gallery UI of Netflix's Website, in this
-                  project I use TMDB API
+                <p className="text-textColor text-md max-w-sm ">
+                  In this project, I have created a user interface inspired by
+                  Netflix's Movie Gallery. To populate the gallery with movie
+                  data, I have integrated the TMDB API.
                 </p>
                 <div className="py-4 flex">
                   <FaReact
@@ -140,17 +163,21 @@ const Projects = () => {
           </div>
 
           {/* Project 3 */}
-          <div className="my-10">
+          {/* <div className="my-10 w-[400px]">
             <div className="w-70 content-center">
-              <div>
+              <a
+                href="https://nerwinalamas.github.io/Product-Preview-Card-Component/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <img
                   src={product}
                   alt="Product Preview Component"
-                  className="rounded-lg transform hover:scale-110 transition duration-300 md:h-25"
+                  className="rounded-lg transform hover:scale-110 transition duration-300"
                 />
-              </div>
+              </a>
               <div className="px-3">
-                <h2 className="text-nameColor font-medium text-xl text-center py-5">
+                <h2 className="text-nameColor font-medium text-xl text-center  pt-5 pb-3">
                   Product Preview Component
                 </h2>
                 <p className="text-textColor text-md max-w-sm">
@@ -191,49 +218,53 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
 
           {/* Project 4 */}
           <div className="my-10">
             <div className="w-70 content-center">
-              <div>
+              <a
+                href="https://github.com/nerwinalamas/GAVS-Smart-Attendance-App"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <img
-                  src={notes}
-                  alt="Notes App"
-                  className="rounded-lg transform hover:scale-110 transition duration-300 md:h-25"
+                  src={Gavs}
+                  alt="GAVS Smart Attendance App"
+                  className="rounded-lg transform hover:scale-110 transition duration-300"
                 />
-              </div>
+              </a>
               <div className="px-3">
-                <h2 className="text-nameColor font-medium text-xl text-center py-5">
-                  Notes App
+                <h2 className="text-nameColor font-medium text-xl text-center  pt-5 pb-3">
+                  GAVS Smart Attendance App
                 </h2>
-                <p className="text-textColor text-md max-w-sm">
-                  A very simple NodeJS CRUD App, users can write notes
+                <p className="text-textColor text-md max-w-sm ">
+                  Due to the strict implementation of social distancing, we have
+                  developed an Attendance App for the Institution that collects
+                  student attendance. Our app utilizes a QR code system to
+                  gather student data.
                 </p>
                 <div className="py-4 flex">
-                  <FaNodeJs
-                    title="NodeJS"
+                  <FaJava
+                    title="Java"
                     className="text-titleColor text-4xl mr-2 hover:text-btnColor"
                   />
-                  <SiExpress
-                    title="ExpressJS"
+                  <DiAndroid
+                    title="Android Studio"
                     className="text-titleColor text-4xl mr-2 hover:text-btnColor"
                   />
-                  <SiMongodb
-                    title="MongoDB"
-                    className="text-titleColor text-4xl mr-2 hover:text-btnColor"
-                  />
-                  <FaBootstrap
-                    title="Bootstrap"
+                  <SiFirebase
+                    title="Firebase"
                     className="text-titleColor text-4xl mr-2 hover:text-btnColor"
                   />
                 </div>
                 <p className="text-textColor">
-                  NodeJS, ExpressJS, EJS, Mongoose & Bootstrap
+                  Java, XML, Android Studio & Firebase
                 </p>
+
                 <div className="mt-2">
                   <a
-                    href="https://github.com/nerwinalamas/Notes"
+                    href="https://github.com/nerwinalamas/GAVS-Smart-Attendance-App"
                     target="_blank"
                     rel="noreferrer"
                   >
